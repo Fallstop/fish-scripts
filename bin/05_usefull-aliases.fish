8db5fc1fda33df2c38d38182b1079cf8fb6accb1
Jasper Miller-Waugh <contact@jmw.nz>
2021-08-30 16:43:15 +1200

    Fixed lla and lal aliases

--- bin/05_usefull-aliases.fish
+++ bin/05_usefull-aliases.fish
@@ -1,11 +1,13 @@
 alias ls="exa"
 alias ll="exa -l"
 alias la="exa -a"
-alias lal="exa -a"
+alias lal="exa -al"
 alias lla="lal"
 alias ld="exa -D"
 alias ldl="exa -Dl"
 alias lld="ldl"
+alias l=ls
+
 alias cdnt="cd -"
 
 if test -e "/etc/arch-release"
