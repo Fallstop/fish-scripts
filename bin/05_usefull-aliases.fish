4844b4f16ff09e29056aee3ffabd2206378b36a2
Jasper Miller-Waugh <contact@jmw.nz>
2021-09-19 19:52:37 +1200

    Refactored ls exa mappings

--- bin/05_usefull-aliases.fish
+++ bin/05_usefull-aliases.fish
@@ -1,12 +1,14 @@
-alias ls="exa"
-alias ll="exa -l"
-alias la="exa -a"
-alias lal="exa -al"
-alias lla="lal"
-alias ld="exa -D"
-alias ldl="exa -Dl"
+alias ls="exa --icons --time-style long-iso"
+alias ll="ls -l"
+alias la="ls -a"
+alias lal="ls -al"
+alias lla="ls"
+alias ld="ls -D"
+alias ldl="ls -Dl"
 alias lld="ldl"
 alias l=ls
+
+alias lll="ls -lTR"
 
 alias cdnt="cd -"
 
