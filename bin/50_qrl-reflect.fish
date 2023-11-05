92f7c931b1c80ce0630edce3dfbb5a09a6c90088
Jasper M-W <contact@jmw.nz>
2023-11-05 16:32:25 +1300

    Disable reverse IP lookup in QRL Reflect script

--- bin/50_qrl-reflect.fish
+++ bin/50_qrl-reflect.fish
@@ -1,5 +1,5 @@
 function qrl-reflect-on
-    if test (sudo iptables -t nat --list | grep 192.168.20.2 | wc -l) = "0";
+    if test (sudo iptables -t nat -nL | grep 192.168.20.2 | wc -l) = "0";
         sudo iptables -t nat -A OUTPUT -p tcp -d 180.222.68.197  -j DNAT --to-destination 192.168.20.2
     else;
         echo "Rule Alredy Added"
@@ -7,7 +7,7 @@ function qrl-reflect-on
 end
 
 function qrl-reflect-off
-    if test (sudo iptables -t nat --list | grep 192.168.20.2 | wc -l) = "0";
+    if test (sudo iptables -t nat -nL | grep 192.168.20.2 | wc -l) = "0";
         echo "Rule Alredy Removed"
     else
         sudo iptables -t nat -D OUTPUT -p tcp -d 180.222.68.197  -j DNAT --to-destination 192.168.20.2
